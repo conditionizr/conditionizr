7972e8388f7f402298d74a433207d9a1261ef278
Sascha Fuchs <gisu2onic@gmail.com>
2015-12-19 14:13:50 +0100

    Modified IE10 Check

--- detects/ie10.js
+++ detects/ie10.js
@@ -3,4 +3,4 @@
  * @cc_on Conditional Compilation to test the
  * JavaScript version and MSIE 10 in the UserAgent
  */
-conditionizr.add('ie10', !!(Function('/*@cc_on return (/^10/.test(@_jscript_version) && /MSIE 10\.0(?!.*IEMobile)/i.test(navigator.userAgent)); @*/')()));
+conditionizr.add('ie10', !!(Function('/*@cc_on return document.documentMode===10@*/')()));
